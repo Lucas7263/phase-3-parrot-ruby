ad292823fa0d9141eb79e03b7dc2e2c1a61fb5d2
Lucas <lucaswoodyard@yahoo.com>
2023-03-20 10:24:27 -0500

    commit tree ebe18a4751240655231a72801bfb42c27c1138d2

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+require 'pry'
+
+
+def parrot(caw = "Squawk!")
+    puts caw
+    return caw
+    binding.pry
+end
+
+parrot()
